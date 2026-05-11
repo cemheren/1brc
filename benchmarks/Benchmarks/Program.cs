2ca2b08ce1631d2f7096530b51de71c155bbef87
Akif Heren <akhe@microsoft.com>
2026-05-11 10:28:45 -0700

    remove byte array solution

--- benchmarks/Benchmarks/Program.cs
+++ benchmarks/Benchmarks/Program.cs
@@ -19,7 +19,6 @@ BenchmarkRunner.Run<ParsingBenchmarks>();
 public class ParsingBenchmarks
 {
     private string[] _lines = null!;
-    private byte[][] _lineBytes = null!;
 
     [GlobalSetup]
     public void Setup()
@@ -36,8 +35,6 @@ public class ParsingBenchmarks
             "swedencentral;158.33",
             "brazilsouth;170.67",
         ];
-
-        _lineBytes = _lines.Select(System.Text.Encoding.UTF8.GetBytes).ToArray();
     }
 
     [Benchmark(Baseline = true)]
@@ -62,43 +59,6 @@ public class ParsingBenchmarks
         {
             int sep = line.IndexOf(';');
             result = (line[..sep], double.Parse(line.AsSpan(sep + 1), provider: CultureInfo.InvariantCulture));
-        }
-        return result;
-    }
-
-    [Benchmark]
-    public (int nameLen, double value) SpanByteParsing()
-    {
-        // Advanced: work entirely with byte spans, no string allocations
-        (int, double) result = default;
-        foreach (var bytes in _lineBytes)
-        {
-            var span = bytes.AsSpan();
-            int sep = span.IndexOf((byte)';');
-            var nameSpan = span[..sep];
-            var valueSpan = span[(sep + 1)..];
-
-            // Hand-rolled decimal parse from ASCII bytes
-            double value = 0;
-            int decimalPos = -1;
-            for (int i = 0; i < valueSpan.Length; i++)
-            {
-                byte b = valueSpan[i];
-                if (b == '.')
-                {
-                    decimalPos = i;
-                    continue;
-                }
-                value = value * 10 + (b - '0');
-            }
-            if (decimalPos >= 0)
-            {
-                int decimals = valueSpan.Length - decimalPos - 1;
-                for (int i = 0; i < decimals; i++)
-                    value /= 10.0;
-            }
-
-            result = (nameSpan.Length, value);
         }
         return result;
     }
